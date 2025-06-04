8d45cae1f9a792655e3d38fd573cd39d48c9719f
vhfantes-Dev <vhfantes.dev@gmail.com>
2025-06-04 17:33:40 -0300

    :sparkles: feat: refactor Usuario model to extend Authenticatable and include Notifiable and HasApiTokens traits

--- Ezcommerce-back/app/Models/Usuario.php
+++ Ezcommerce-back/app/Models/Usuario.php
@@ -1,15 +1,23 @@
 <?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
 
-class Usuario extends Model
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Usuario extends Authenticatable
 {
+    use HasApiTokens, Notifiable;
+
+    protected $table = 'usuarios'; // se sua tabela no banco for 'usuarios'
+    
     protected $fillable = ['nome', 'email', 'senha'];
 
-    public function pedidos() {
+    protected $hidden = ['senha', 'remember_token']; // oculta a senha nas respostas JSON
+
+    public function pedidos()
+    {
         return $this->hasMany(Pedido::class, 'id_user_fk');
     }
 }
-
-
-?>
