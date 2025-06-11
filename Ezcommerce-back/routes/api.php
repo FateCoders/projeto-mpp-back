3358bf16fb920b2418b0729a721309d53102f25f
vhfantes-Dev <vhfantes.dev@gmail.com>
2025-06-11 15:48:10 -0300

    :sparkles: feat: adicionando as rotas do produto

--- Ezcommerce-back/routes/api.php
+++ Ezcommerce-back/routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\PedidoController;
 use App\Http\Controllers\UsuarioController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TokenController;
+use App\Http\Controllers\ProdutoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,10 +25,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/realizar-pedido', [PedidoController::class, 'realizarPedido']);
 Route::post('/pedidos/{id}/confirmar', [PedidoController::class, 'confirmarCompra']);
 Route::get('/pedido/{id}', [PedidoController::class, 'verPedido']);
-Route::get('/pedidos/usuario/{id}', [PedidoController::class, 'verPedidosByUserId']);
+Route::get('/pedidos/usuario/{id}', [PedidoController::class, 'produtosComprados']);
 // routes/api.php
 Route::post('/registrar', [UsuarioController::class, 'registrar']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/produtos', [ProdutoController::class, 'index']);
+Route::get('/produtos/categoria/{categoria}', [ProdutoController::class, 'porCategoria']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/usuario', [AuthController::class, 'usuario']);
